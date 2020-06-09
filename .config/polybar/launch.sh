6c358babead3b84c9ea3c6a03960df6209824ee3
ktkk <dezuttereluka@gmail.com>
2020-06-09 23:56:51 +0200

    Added custom config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch example bar
-ech "---" | tee -a /tmp/polybar1.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+# Launch main bar
+echo "---" | tee -a /tmp/polybar1.log
+polybar main -r >>/tmp/polybar1.log 2>&1 &
 
 echo "Bars launched ..."
