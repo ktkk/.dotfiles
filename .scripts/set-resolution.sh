e5c93a92593a5e10134ff536bef852322657c4fc
ktkk <dezuttereluka@gmail.com>
2020-06-09 23:44:44 +0200

    Added scripts directory

---
+++ .scripts/set-resolution.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add new mode
+xrandr --newmode "1920x1080" 172.80 1920 2040 2248 2576 1080 1081 1084 1118 -Hsync +Vsync
+# Add new mode to display
+xrandr --addmode Virtual-1 1920x1080
+# Set new mode
+xrandr --output Virtual-1 --mode 1920x1080
