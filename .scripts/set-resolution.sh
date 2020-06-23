15dd86afb3851d7c4272efcc067fef0043fbe5e4
ktkk <dezuttereluka@gmail.com>
2020-06-23 21:18:21 +0200

    Added comments

--- .scripts/set-resolution.sh
+++ .scripts/set-resolution.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This only works in VirtualBox
+# Remove or edit for bare metal
+
 # Add new mode
 xrandr --newmode "1920x1080" 172.80 1920 2040 2248 2576 1080 1081 1084 1118 -Hsync +Vsync
 # Add new mode to display
