34ed27a4f3fc890242b5e9ac220b32efffaf0a8e
ktkk <dezuttereluka@gmail.com>
2020-07-23 23:52:35 +0200

    It's working

--- .scripts/toggle-theme.sh
+++ .scripts/toggle-theme.sh
@@ -6,14 +6,42 @@ BSPWMRC="$HOME/.config/bspwm/bspwmrc"
 BG=$($HOME/.scripts/find-colors.sh background)
 THEME=$1
 
-if [ "$THEME" = "light" ]; then
-	echo "LIGHT!"
-	echo $BG
-elif [ "$THEME" = "dark" ]; then
-	echo "DARK!"
-	echo $BG
-fi
+case $THEME in
+	light)
+		echo "LIGHT!"
+		if [ "$BG" = "#dfdfdf" ]; then
+			echo "Already in light mode!"
+		elif [ "$BG" = "#222222" ]; then
+			sed -in "s/background: #222222/background: #dfdfdf/g" $COLORS
+			sed -in "s/foreground: #dfdfdf/foreground: #222222/g" $COLORS
+			#nitrogen --set-scaled $HOME/Pictures/Wallpapers/golden-gate.jpg
+			feh --bg-scale $HOME/Pictures/Wallpapers/golden-gate.jpg 
+			echo "Set background color to white."
+		fi
+		;;
+	dark)
+		echo "DARK!"
+		if [ "$BG" = "#222222" ]; then
+			echo "Already in dark mode!"
+		elif [ "$BG" = "#dfdfdf" ]; then
+			sed -in "s/background: #dfdfdf/background: #222222/g" $COLORS
+			sed -in "s/foreground: #222222/foreground: #dfdfdf/g" $COLORS
+			#nitrogen --set-scaled $HOME/Pictures/Wallpapers/mount-fuji.jpg
+			feh --bg-scale $HOME/Pictures/Wallpapers/mount-fuji.jpg 
+			echo "Set background color to black."
+		fi
+		;;
+esac
 
-#xrdb $HOME/.Xresources &
-#$HOME/.config/polybar/launch.sh &
-#$HOME/.config/bspwm/bspwmrc &
+xrdb -merge $HOME/.Xresources
+$HOME/.config/polybar/launch.sh
+
+sed -in 4's/.*/#&/' $BSPWMRC
+sed -in 7's/.*/#&/' $BSPWMRC
+
+$HOME/.config/bspwm/bspwmrc &
+
+sed -in 4's/^.//' $BSPWMRC
+sed -in 7's/^.//' $BSPWMRC
+
+exit
