2b0b9cae9b7bb0e6ca0b0cb2b103d15779690464
ktkk <dezuttereluka@gmail.com>
2020-07-23 19:34:54 +0200

    Updated WIP toggle script

--- .scripts/toggle-theme.sh
+++ .scripts/toggle-theme.sh
@@ -2,26 +2,18 @@
 
 COLORS="$HOME/.config/xresources/colors"
 BSPWMRC="$HOME/.config/bspwm/bspwmrc"
+
+BG=$($HOME/.scripts/find-colors.sh background)
 THEME=$1
 
 if [ "$THEME" = "light" ]; then
-	sed "s/*background: #dfdfdf/*background: #dfdfdf/g" $COLORS &
-	sed "s/*background: #222222/*background: #dfdfdf/g" $COLORS &
-	sed "s/*foreground: #222222/*foreground: #222222/g" $COLORS &
-	sed "s/*foreground: #dfdfdf/*foreground: #222222/g" $COLORS &
+	echo "LIGHT!"
+	echo $BG
 elif [ "$THEME" = "dark" ]; then
-	sed "s/*background: #222222/*background: #222222/g" $COLORS &
-	sed "s/*background: #dfdfdf/*background: #222222/g" $COLORS &
-	sed "s/*foreground: #dfdfdf/*foreground: #dfdfdf/g" $COLORS &
-	sed "s/*foreground: #222222/*foreground: #dfdfdf/g" $COLORS &
+	echo "DARK!"
+	echo $BG
 fi
 
-sed "s/sxhkd -c $HOME/.config/sxhkd/sxhkdrc &/#sxhkd -c $HOME/.config/sxhkd/sxhkdrc &/g" $BSPWMRC &&
-sed "s/$HOME/.config/bspwm/autostart.sh/#$HOME/.config/bspwm/autostart.sh/g" $BSPWMRC &
-
-xrdb $HOME/.Xresources &
-$HOME/.config/polybar/launch.sh &
-$HOME/.config/bspwm/bspwmrc &
-
-sed "s/#sxhkd -c $HOME/.config/sxhkd/sxhkdrc &/sxhkd -c $HOME/.config/sxhkd/sxhkdrc &/g" $BSPWMRC &&
-sed "s/#$HOME/.config/bspwm/autostart.sh/$HOME/.config/bspwm/autostart.sh/g" $BSPWMRC &
+#xrdb $HOME/.Xresources &
+#$HOME/.config/polybar/launch.sh &
+#$HOME/.config/bspwm/bspwmrc &
